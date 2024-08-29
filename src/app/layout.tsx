f09ad55c0ce50dfd423d2fc5d1725a3823148456
aisl1n <aislanarchy@gmail.com>
2024-08-29 20:34:43 -0300

    fix: Adjust the menuNavigation buttons to have padding on top

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,13 +27,15 @@ export default function RootLayout({
           <header className="flex flex-row justify-between p-2">
             <Link href="/">
               <Button variant="outline" className="mr-2">
-                <House size={"19px"}/>
+                <House size={"19px"} />
               </Button>
             </Link>
             <ThemeToggle />
           </header>
           {children}
-          <MenuNavigation />
+          <div className="p-20">
+            <MenuNavigation />
+          </div>
         </ThemeProvider>
       </body>
     </html>
